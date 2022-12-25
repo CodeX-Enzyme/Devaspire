2aaf11ad8d227f47af75846f336be9b1a2aaec9a
Riju Basu <whitedominator17@gmail.com>
2022-12-25 16:13:18 +0530

    About sectionupdate and few moderation

--- js/script.js
+++ js/script.js
@@ -2,3 +2,23 @@ const navimg = document.querySelector("#navimg") ;
 navimg.addEventListener("click",()=>{
     window.location.replace("https://devaspire.live") ;
 })
+
+const section = document.querySelectorAll('section');
+const navli = document.querySelectorAll('nav ul li');
+
+window.addEventListener('scroll', () => {
+    let current = '';
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+        if (scrollY >= (sectionTop - sectionHeight / 3)) {
+            current = section.getAttribute('id');
+        }
+    })
+    navli.forEach( li => {
+        li.classlist.remove('active');
+        if (li.classlist.contains(current)) {
+            li.classlist.add('active');
+        }
+    })
+})
